4cf5c2a2eeb6eb81f2985de51be102fd2797e7f4
Alexander Boythenko <banzalik@gmail.com>
2012-02-23 18:30:23 +0200

    переезд на новый bem-mini

--- bem-mini/blocks/.bem/level.js
+++ bem-mini/blocks/.bem/level.js
@@ -1,9 +1,9 @@
 var myPath = require('bem/lib/path');
 
 exports.techs = {
-    'js': '../blocks/i-bem/bem/techs/js.js',
+    'js': '../i-bem/bem/techs/js.js',
     'css': 'bem/lib/techs/css',
-    'bemhtml': '../blocks/i-bem/bem/techs/bemhtml.js',
+    'bemhtml': '../i-bem/bem/techs/bemhtml.js',
     'less': '../.bem/techs/less.js'
 };
 
